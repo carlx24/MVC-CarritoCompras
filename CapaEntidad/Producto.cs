046a42bac2bd150d210e9505f0cea0c601e4fe07
Carlos <Carlos@KENSHIN>
2023-08-04 16:50:30 -0400

    Agregar archivos de proyecto.

---
+++ CapaEntidad/Producto.cs
@@ -0,0 +1,37 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Marca oMarca { get; set; }
+
+        public Categoria oCategoria { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int Stock { get; set; }
+
+        public string RutaImagen { get; set; }
+
+        public string NombreImagen { get; set; }
+
+        public bool  Activo { get; set; }
+
+    }
+}
+
+
---
+++ CapaEntidad/Venta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Venta
+    {
+        public int IdVenta { get; set; }
+
+        public int IdCliente { get; set; }
+
+        public int TotalProducto { get; set; }
+
+        public int MontoTotal { get; set; }
+    }
+}
